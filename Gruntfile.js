bfab6ed4da508c8896158d6a8f62683e623838b8
wangding <408542507@qq.com>
2019-03-13 16:31:01 +0800

    mocha-bev0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,44 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
-    eslint: {
-      options: {
-                configFile: '.eslintrc.json'
+    grunt.initConfig({
+      =    mochacli: {
+        options: {
+                  reporter: 'spec',
+            bail: true
+            
+        },
+          all: ['test/*.js']
+                           
+      },
+      mocha_istanbul: {
+      coverage: {
+              src: 'test'
+                    
+      }
           
       },
-        target: ['*.js']
+      istanbul_check_coverage: {
+      default: {
+      options: {
+                coverageFolder: 'coverage*',
+                check: {
+                            lines: 90,
+                                        statements: 90
+                                                  
+                }
+                        
+      }
+            
+      }
+          
+      }
         
-    }
-              
-  });
+    });
 
-    grunt.loadNpmTasks('grunt-eslint');
+      grunt.loadNpmTasks('grunt-mocha-cli');
+        grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-      grunt.registerTask('default', ['eslint']);
+          grunt.registerTask('default', ['mochacli']);   
+            grunt.registerTask('cover', ['mocha_istanbul']);
+              grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 
 };
