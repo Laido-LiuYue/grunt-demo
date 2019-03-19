a324220350ab703cca27d016749cd8d932775c75
Laido-LiuYue <578201842@qq.com>
2019-03-19 10:25:59 +0800

    js压缩实例

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    cssmin: {  
-      'rectangle.min.css': 'rectangle.css'
-    }              
+    uglify: {
+      release:{
+        files: {
+          'js/jquery.min.js': 'js/jquery.js'    
+        }            
+      }           
+    }    
   });
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', ['cssmin']); 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify:release']); 
 };
